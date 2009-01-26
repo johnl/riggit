a367774cd17b2e39bbd8a52efd6b90c5319428ae
John Leach <john@johnleach.co.uk>
2009-01-26 20:23:20 +0000

    app/models/user_observer.rb: Fix after_create failure if no site has been defined

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -1,6 +1,7 @@
 class UserObserver < ActiveRecord::Observer
   def after_create(user)
-    @current_site = Site.find(:first)
-    deliver_useer_notification(user, @current_site.admin_email) if !@current_site.admin_email.blank? && @current_site.notify_new_user
+    if @current_site = Site.find(:first)
+      deliver_useer_notification(user, @current_site.admin_email) if @current_site.admin_email? && @current_site.notify_new_user
+    end
   end
 end
